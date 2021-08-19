5fc06614cca8c45d555af731a2b9dcef9104dbe1
Fermin Aguilar <ferminab99@gmail.com>
2021-08-19 17:35:47 -0300

    se agrego archivo de condicionales

---
+++ js/ejercicios1-5.js
@@ -0,0 +1,10 @@
+// ejercicio 5
+
+// parseInt("numero") --> transforma cadena de texto en numero entero
+let numero1 = parseInt (prompt("Ingrese un primer numero"))
+let numero2 = parseInt(prompt("ingrese un segundo numero"));
+document.write("<br> El resultado de la suma es: " + (numero1 + numero2))
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero1 + numero2)
