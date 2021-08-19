10dc3faf6e639b9a450c0a5d0942ead7506b3e55
Fermin Aguilar <ferminab99@gmail.com>
2021-08-19 16:35:07 -0300

    se agrego archivo de ejercicios 1 al 5

---
+++ js/holaMundo.js
@@ -0,0 +1,28 @@
+
+// comentario simple
+
+/*esto es 
+un comentario
+de varias lineas*/
+
+// Mostrar un mensaje por consola
+console.log("Hola mundo");
+
+// Mostrar un mensaje en la pagina html
+document.write("Este es mi primer mensaje")
+
+// mostrar una ventana emergente
+// alert("Esto es una alerta")
+
+// crear o declarar una variable var - let - const
+
+let palabraBuscada = "hola"; 
+
+document.write("<br>Variable palabra buscada: " + palabraBuscada);
+
+// modificar el valor de una variable
+palabraBuscada = "pagina de blog";
+document.write("<br>Variable palabra buscada: " + palabraBuscada);
+
+const comision = 17;
+document.write("<br>Constante comision: " + comision);
